1be1888db390c257b442270a927c373a589f96bc
Maxim <pingwin106@gmail.com>
2023-02-13 17:35:53 +0300

    change blogs

--- src/validators/validation.ts
+++ src/validators/validation.ts
@@ -2,14 +2,14 @@ import {body, validationResult} from "express-validator";
 
 
 export const nameValidation = body('name', ).trim().isLength({
-
+    min: 1,
     max: 15
 }).withMessage('title')
 export const descriptionValidate = body('description', ).trim().isLength({
-
+    min: 1,
     max: 500
 }).withMessage('description')
 export const websiteUrlValidate = body('websiteUrl', ).trim().isLength({
-
+    min: 1,
     max: 100
 }).isURL({protocols: ['https']}).withMessage('websiteUrl')
