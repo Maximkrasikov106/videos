2333cca8572c489298946f90021d0082417d425a
Maxim <pingwin106@gmail.com>
2023-02-13 18:03:14 +0300

    change blogs

--- src/midlewares/input-validation-middleware.ts
+++ src/midlewares/input-validation-middleware.ts
@@ -2,20 +2,22 @@ import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 
 export const inputValidationMiddleware = (req: Request, res:Response, next: NextFunction) => {
+    const errorFormatter = ({ location, msg, param, value, nestedErrors } : any) => {
+        // Build your resulting errors however you want! String, object, whatever - it works!
+        return {
+            message: msg,
+            field: param
+        };
+    };
 
 
 
-
-    let myValidationResult = validationResult(req)
+    let myValidationResult = validationResult(req).formatWith(errorFormatter)
 
 
     if(!myValidationResult.isEmpty()) {
-        const errorsArray = myValidationResult.array().map(e => (
-            {
-                message: e.msg,
-                field: e.param
-        }));
-        res.status(400).send({errorsMessages: errorsArray })
+
+        res.status(400).send({errorsMessages: myValidationResult.array({onlyFirstError: true}) })
     }else{
         next()
     }
--- src/validators/validation.ts
+++ src/validators/validation.ts
@@ -9,7 +9,7 @@ export const descriptionValidate = body('description', ).trim().isLength({
     min: 1,
     max: 500
 }).withMessage('description')
-export const websiteUrlValidate = body('websiteUrl', ).trim().isLength({
-    min: 1,
+export const websiteUrlValidate = body('websiteUrl', ).exists().isString().trim().notEmpty().isLength({
+
     max: 100
-}).isURL({protocols: ['https']}).withMessage('websiteUrl')
+}).isURL().withMessage('websiteUrl')
