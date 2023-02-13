ee6378093726426ad648050b5be0b9551c53ffa2
Maxim <pingwin106@gmail.com>
2023-02-13 16:16:06 +0300

    change blogs

--- src/midlewares/input-validation-middleware.ts
+++ src/midlewares/input-validation-middleware.ts
@@ -2,7 +2,7 @@ import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 
 export const inputValidationMiddleware = (req: Request, res:Response, next: NextFunction) => {
-    const myValidationResult = validationResult(req.body.msg)
+    const myValidationResult = validationResult(req)
 
     if(!myValidationResult.isEmpty()) {
         res.status(400).send({errorsMessages: myValidationResult })
