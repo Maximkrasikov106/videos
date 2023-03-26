03382db144f71f423318923822d74d5adcabd76b
Maxim <pingwin106@gmail.com>
2023-03-26 09:35:24 +0300

    add new Validate forparams

--- src/routes/testing-routes.ts
+++ src/routes/testing-routes.ts
@@ -6,7 +6,7 @@ export const testingRouter = Router({})
 
 
 testingRouter.delete('/all-data', (req, res)=> {
-     client.db("soc").collection<BlogsType>("blogs").deleteMany({})
-     client.db("soc").collection<PostType>("posts").deleteMany({})
+     client.db("soc").collection("blogs").deleteMany({})
+     client.db("soc").collection("posts").deleteMany({})
     res.sendStatus(204)
 });
